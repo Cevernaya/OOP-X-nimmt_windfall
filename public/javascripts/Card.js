8d3d321b7fd383591b2dc630841bd5c3c8d8a1c2
windfall <1998pcs@naver.com>
2021-01-12 02:27:53 +0900

    Initial setting

---
+++ public/javascripts/Card.js
@@ -0,0 +1,16 @@
+class Card {
+    constructor (order) {
+        this.number = order
+        this.point = setPoint(order)
+    }
+}
+
+exports.Card = Card
+
+function setPoint(num){
+    if (num == 55) return 7
+    else if (num % 11 == 0) return 5
+    else if (num % 10 == 0) return 3
+    else if (num % 5 == 0) return 2
+    else return 1
+}
